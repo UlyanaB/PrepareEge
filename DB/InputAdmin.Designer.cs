9ae966342f5ea6324c83d25de7b236460d83d2f9
bb <bba2@yandex.ru>
2020-06-21 16:39:18 +1000

    InputAdmin

---
+++ DB/InputAdmin.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace DB
+{
+    partial class InputAdmin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnInput = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TxLogin = new System.Windows.Forms.TextBox();
+            this.TxPassword = new System.Windows.Forms.TextBox();
+            this.BtnCancel = new System.Windows.Forms.Button();
+            this.LbMessage = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // BtnInput
+            // 
+            this.BtnInput.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.BtnInput.Location = new System.Drawing.Point(40, 119);
+            this.BtnInput.Name = "BtnInput";
+            this.BtnInput.Size = new System.Drawing.Size(75, 23);
+            this.BtnInput.TabIndex = 3;
+            this.BtnInput.Text = "Вход";
+            this.BtnInput.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Логин";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Пароль";
+            // 
+            // TxLogin
+            // 
+            this.TxLogin.Location = new System.Drawing.Point(69, 13);
+            this.TxLogin.Name = "TxLogin";
+            this.TxLogin.Size = new System.Drawing.Size(100, 20);
+            this.TxLogin.TabIndex = 1;
+            // 
+            // TxPassword
+            // 
+            this.TxPassword.Location = new System.Drawing.Point(69, 50);
+            this.TxPassword.Name = "TxPassword";
+            this.TxPassword.Size = new System.Drawing.Size(100, 20);
+            this.TxPassword.TabIndex = 2;
+            // 
+            // BtnCancel
+            // 
+            this.BtnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.BtnCancel.Location = new System.Drawing.Point(122, 119);
+            this.BtnCancel.Name = "BtnCancel";
+            this.BtnCancel.Size = new System.Drawing.Size(75, 23);
+            this.BtnCancel.TabIndex = 4;
+            this.BtnCancel.Text = "Cancel";
+            this.BtnCancel.UseVisualStyleBackColor = true;
+            // 
+            // LbMessage
+            // 
+            this.LbMessage.AutoSize = true;
+            this.LbMessage.Location = new System.Drawing.Point(12, 151);
+            this.LbMessage.Name = "LbMessage";
+            this.LbMessage.Size = new System.Drawing.Size(35, 13);
+            this.LbMessage.TabIndex = 6;
+            this.LbMessage.Text = "label3";
+            // 
+            // InputAdmin
+            // 
+            this.AcceptButton = this.BtnInput;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.BtnCancel;
+            this.ClientSize = new System.Drawing.Size(235, 173);
+            this.ControlBox = false;
+            this.Controls.Add(this.LbMessage);
+            this.Controls.Add(this.BtnCancel);
+            this.Controls.Add(this.TxPassword);
+            this.Controls.Add(this.TxLogin);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.BtnInput);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "InputAdmin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "InputAdmin";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnInput;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        internal System.Windows.Forms.TextBox TxLogin;
+        internal System.Windows.Forms.TextBox TxPassword;
+        private System.Windows.Forms.Button BtnCancel;
+        internal System.Windows.Forms.Label LbMessage;
+    }
+}
--- DB/MainForm.cs
+++ DB/MainForm.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -13,6 +14,8 @@ namespace DB
 {
     public partial class MainForm : Form
     {
+        private InputAdmin inputAdmin = null;
+
         internal NpgsqlConnection con = null;
         internal NpgsqlDataAdapter da;
         internal DataSet ds = new DataSet();
@@ -37,14 +40,86 @@ namespace DB
 
         private void MainForm_Load(object sender, EventArgs e)
         {
-            con = new NpgsqlConnection("Server=localhost;Port=5432; User Id=postgres;Password=postgres; Database=school;");
-            con.Open();
+            bool flg = true;
+            bool continueFlg = false;
+            string connectionString = @"Data Source=DESKTOP-N3D8F06\SQLEXPRESS; Initial Catalog = AdminDB; Integrated Security = True";
+            int? rslt = null;
+            string selectCmd = @"select 1 from adminDB a where a.logina = @lgn and a.passworda = @psw";
+
+            using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+            {
+                sqlConnection.Open();
+                if (sqlConnection.State != ConnectionState.Open)
+                {
+                    MessageBox.Show("Невозможно подключииться к БД MS SQL");
+                    return;
+                }
+
+                using (SqlCommand checkLogin = new SqlCommand(selectCmd, sqlConnection))
+                {
+                    checkLogin.Parameters.Add("lgn", SqlDbType.NVarChar, 20);
+                    checkLogin.Parameters.Add("psw", SqlDbType.NVarChar, 20);
+
+                    using (inputAdmin = new InputAdmin())
+                    {
+                        while (flg)
+                        {
+                            DialogResult dr = inputAdmin.ShowDialog();
+
+                            if (dr == DialogResult.Cancel)
+                            {
+                                flg = false;
+                            }
+                            else if (dr == DialogResult.OK)
+                            {
+                                string lgn = inputAdmin.TxLogin.Text.Trim();
+                                string psw = inputAdmin.TxPassword.Text.Trim();
+
+                                checkLogin.Parameters["lgn"].Value = lgn;
+                                checkLogin.Parameters["psw"].Value = psw;
+                                rslt = checkLogin.ExecuteScalar() as int?;
+                                if (rslt.HasValue && rslt.Value == 1)
+                                {
+                                    flg = false;
+                                    continueFlg = true;
+                                    inputAdmin.Close();
+                                    // записать в протокол
+                                }
+                                else
+                                {
+                                    inputAdmin.LbMessage.Text = "Вы ошиблись в логине или пароле";
+                                    // записать в протокол !!!
+                                }
+
+                            }
+                            else
+                            {
+                                inputAdmin.LbMessage.Text = "Ошибка программы № 1";
+                                return;
+                            }
+                        }
+                    }
+                }
+
+                if (continueFlg)
+                {
+                    con = new NpgsqlConnection("Server=localhost;Port=5432; User Id=postgres;Password=postgres; Database=school;");
+                    con.Open();
+                }
+                else
+                {
+                    Close();
+                }
+            }
         }
 
         private void MainForm_FormClosed(object sender, FormClosedEventArgs e)
         {
-            con.Close();
-            con.Dispose();
+            if (con != null)
+            {
+                con.Close();
+                con.Dispose();
+            }
         }
     }
 }
