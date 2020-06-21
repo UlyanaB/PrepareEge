855f368d6dce8ec105224fbd75f532ff89efc5f9
bb <bba2@yandex.ru>
2020-06-21 17:21:05 +1000

    Ввод даты и системы в базу данных

--- DB/MainForm.cs
+++ DB/MainForm.cs
@@ -14,8 +14,12 @@ namespace DB
 {
     public partial class MainForm : Form
     {
-        private InputAdmin inputAdmin = null;
+        const string connectionString = @"Data Source=DESKTOP-N3D8F06\SQLEXPRESS; Initial Catalog = AdminDB; Integrated Security = True";
 
+        private InputAdmin inputAdmin = null;
+        private SqlConnection sqlConnection = null;
+
+        internal int IdAdmin;
         internal NpgsqlConnection con = null;
         internal NpgsqlDataAdapter da;
         internal DataSet ds = new DataSet();
@@ -42,11 +46,10 @@ namespace DB
         {
             bool flg = true;
             bool continueFlg = false;
-            string connectionString = @"Data Source=DESKTOP-N3D8F06\SQLEXPRESS; Initial Catalog = AdminDB; Integrated Security = True";
             int? rslt = null;
             string selectCmd = @"select 1 from adminDB a where a.logina = @lgn and a.passworda = @psw";
 
-            using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+            using (sqlConnection = new SqlConnection(connectionString))
             {
                 sqlConnection.Open();
                 if (sqlConnection.State != ConnectionState.Open)
@@ -82,7 +85,8 @@ namespace DB
                                 {
                                     flg = false;
                                     continueFlg = true;
-                                    inputAdmin.Close();
+                                    IdAdmin = getIdAdmin(lgn);
+                                    SetSystemInfo();
                                     // записать в протокол
                                 }
                                 else
@@ -119,6 +123,40 @@ namespace DB
             {
                 con.Close();
                 con.Dispose();
+            }
+        }
+
+        private int getIdAdmin(string lgn)
+        {
+            string selectCmd = @"select id_admin from adminDB a where a.logina = @lgn";
+            int idAdmin;
+
+            using (SqlCommand getAdminId = new SqlCommand(selectCmd, sqlConnection))
+            {
+                getAdminId.Parameters.Add("lgn", SqlDbType.NVarChar, 20);
+                getAdminId.Parameters["lgn"].Value = lgn;
+                idAdmin = (int)getAdminId.ExecuteScalar();
+            }
+            return idAdmin;
+        }
+
+        private void SetSystemInfo()
+        {
+            string machineName = Environment.MachineName;
+            string osName = Environment.OSVersion.ToString();
+
+            string sqlIns = "insert record(id_admin, dataCom, nameCom) values(@id, @dt, @nm)";
+            using (SqlCommand setSysInfo = new SqlCommand(sqlIns, sqlConnection))
+            {
+                setSysInfo.Parameters.Add("id", SqlDbType.Int);
+                setSysInfo.Parameters.Add("dt", SqlDbType.DateTime);
+                setSysInfo.Parameters.Add("nm", SqlDbType.NVarChar, 50);
+
+                setSysInfo.Parameters["id"].Value = IdAdmin;
+                setSysInfo.Parameters["dt"].Value = DateTime.Now;
+                setSysInfo.Parameters["nm"].Value = machineName + ", " +osName;
+
+                setSysInfo.ExecuteNonQuery();
             }
         }
     }
